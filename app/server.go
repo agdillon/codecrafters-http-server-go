1c10baa0c67292ed640f6f94f34898a6188e55b7
Amanda Dillon <agdillon@gmail.com>
2024-07-12 21:34:59 -0600

    Use bufio.Scanner

--- app/server.go
+++ app/server.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 )
 
-func main() {
-	const httpVer = "HTTP/1.1"
+const httpVer = "HTTP/1.1"
 
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -22,23 +23,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	scanner := bufio.NewScanner(conn)
+	_ = scanner.Scan()
+	line := scanner.Text()
+	segments := strings.Split(line, " ")
+	// method := segments[0]
+	target := segments[1]
+	// version := segments[2]
+
 	var statuses = map[int]string{
 		200: "200 OK",
 		404: "404 Not Found",
 	}
-
-	var req = make([]byte, 500)
-	conn.Read(req)
-
-	reqStr := string(req)
-	fmt.Printf("hello, %s", reqStr)
-	lines := strings.Split(reqStr, "\r\n")
-
-	reqLine := lines[0]
-	segments := strings.Split(reqLine, " ")
-	// method := segments[0]
-	target := segments[1]
-	// version := segments[2]
 
 	var statusCode int
 	switch target {
